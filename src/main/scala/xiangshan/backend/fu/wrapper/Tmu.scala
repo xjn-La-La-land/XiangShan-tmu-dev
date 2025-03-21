bf155bacfba4350dddf67be6eb53efecb2718cb3
xjn-La-La-land <xiejianan21@mails.ucas.ac.cn>
2025-03-21 11:24:19 +0800

    minor fixes

--- src/main/scala/xiangshan/backend/fu/wrapper/Tmu.scala
+++ src/main/scala/xiangshan/backend/fu/wrapper/Tmu.scala
@@ -2,11 +2,12 @@ package xiangshan.backend.fu.wrapper
 
 import org.chipsalliance.cde.config.Parameters
 import chisel3._
-import xiangshan.backend.fu.{FuConfig, FuncUnit, TmuDataModule}
+import xiangshan.backend.fu.{FuConfig, FuncUnit, TmuModule}
 import xiangshan.backend.fu.TmuParams
 import xiangshan.backend.fu.FuncUnitIO
 import xiangshan.cache.mmu.TlbRequestIO
 import freechips.rocketchip.tilelink._
+import freechips.rocketchip.diplomacy.LazyModule
 
 // add memory io
 class TmuFuncUnitIO(cfg: FuConfig)(implicit p: Parameters) extends FuncUnitIO(cfg) with TmuParams {
@@ -17,29 +18,30 @@ class TmuFuncUnitIO(cfg: FuConfig)(implicit p: Parameters) extends FuncUnitIO(cf
 class Tmu (cfg: FuConfig)(implicit p: Parameters) extends FuncUnit(cfg) {
   override val io: TmuFuncUnitIO = new TmuFuncUnitIO(cfg)
 
-  private val tmuModule = Module(new TmuDataModule)
-  io.tlb <> tmuModule.io.tlb
-  io.node := tmuModule.io.node
+  private val tmu = LazyModule(new TmuModule())
+  val tmuIO = tmu.module.io
+  io.tlb <> tmuIO.tlb
+  io.node := tmu.clientNode
 
   private val src    = io.in.bits.data.src.take(2) // 2 src
   private val imm    = io.in.bits.data.imm(31, 0)
   private val func   = io.in.bits.ctrl.fuOpType // tmu op
   private val robIdx = io.in.bits.ctrl.robIdx
 
-  tmuModule.io.in.bits.src    := src
-  tmuModule.io.in.bits.imm    := imm
-  tmuModule.io.in.bits.func   := func
-  tmuModule.io.in.bits.robIdx := robIdx
+  tmuIO.in.bits.src    := src
+  tmuIO.in.bits.imm    := imm
+  tmuIO.in.bits.func   := func
+  tmuIO.in.bits.robIdx := robIdx
 
   // uncertain latency
-  tmuModule.io.in.valid := io.in.valid
-  io.in.ready  := tmuModule.io.in.ready
-  io.out.valid := tmuModule.io.out.valid
-  tmuModule.io.out.ready := io.out.ready
+  tmuIO.in.valid := io.in.valid
+  io.in.ready  := tmuIO.in.ready
+  io.out.valid := tmuIO.out.valid
+  tmuIO.out.ready := io.out.ready
 
   io.out.bits := DontCare
-  io.out.bits.res.data   := 0.U  // no write back data for Xtm instruction
-  io.out.bits.ctrl.robIdx := tmuModule.io.out.bits.robIdx
+  io.out.bits.res.data    := 0.U  // no write back data for Xtm instruction
+  io.out.bits.ctrl.robIdx := tmuIO.out.bits.robIdx
 
 }
 
