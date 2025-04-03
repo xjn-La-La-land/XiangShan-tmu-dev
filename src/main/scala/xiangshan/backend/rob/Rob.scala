e96519f609c73c92799bafe1b52776ae302e4646
xjn-La-La-land <xiejianan21@mails.ucas.ac.cn>
2025-04-03 18:04:28 +0800

    m

--- src/main/scala/xiangshan/backend/rob/Rob.scala
+++ src/main/scala/xiangshan/backend/rob/Rob.scala
@@ -456,8 +456,8 @@ class RobImp(override val wrapper: Rob)(implicit p: Parameters, params: BackendP
 
   val dispatchNum = Mux(io.enq.canAccept, PopCount(io.enq.req.map(req => req.valid && req.bits.firstUop)), 0.U)
   io.enq.isEmpty := RegNext(isEmpty && !VecInit(io.enq.req.map(_.valid)).asUInt.orR)
-  io.enq.hasLoadStore := RegNext(hasLoadStore || VecInit(io.enq.req.map(e => e.valid && CommitType.isLoadStore(e.bits.commitType))).asUInt.orR)
-  io.enq.hasTileLS    := RegNext(hasTileLS    || VecInit(io.enq.req.map(e => e.valid && CommitType.isTileLS(e.bits.commitType))).asUInt.orR)
+  io.enq.hasLoadStore := hasLoadStore || VecInit(io.enq.req.map(e => e.valid && CommitType.isLoadStore(e.bits.commitType))).asUInt.orR
+  io.enq.hasTileLS    := hasTileLS    || VecInit(io.enq.req.map(e => e.valid && CommitType.isTileLS(e.bits.commitType))).asUInt.orR
   // io.enq.hasLoadStore := hasLoadStore
   // io.enq.hasTileLS    := hasTileLS
 
